82e049b858d8f4b343117d6c8a9b03184367d626
Amanda Abreu <103958998+Amandaaaaabreu@users.noreply.github.com>
2024-04-04 09:35:01 -0300

    codigo swap

--- listas MOJ/swap.c
+++ listas MOJ/swap.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h> 
 
+void swap(int *a, int *b){
+    int temp; 
+    temp = *a; 
+    *a = *b;
+    *b = temp; 
+}
+/*
 int main(){
+    int a, b;
+
+    printf("Insira dois numeros inteiros:");
+    scanf("%d%d", &a, &b);
+
+    swap(&a, &b);
+
+    printf("Troca realizada: %d %d\n ", a, b);
+
     return 0; 
 }
+*/
+
